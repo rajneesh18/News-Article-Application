d1defd42ecde6f24d5783ea67a09a8ff0574d887
Rajneesh Shukla <shukla.rajneesh2018@gmail.com>
2025-04-25 12:37:36 +0530

    Installing Shadcn UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,9 @@ import { Button } from "./components/ui/button";
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline ">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
       <div className="flex flex-col items-center justify-center min-h-svh">
         <Button>Click me</Button>
       </div>
